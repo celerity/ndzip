66c3ea87e1cb4f7e7b029ca54fee10be111c9859
Fabian Knorr <fabian.knorr@uibk.ac.at>
2022-06-08 16:29:35 +0200

    Enable continuous testing

---
+++ ci/run-unit-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit -o pipefail -o nounset
+
+if [[ ! -d CMakeFiles ]]; then
+    echo "Warning: This script should be run from within a build directory" >&2
+fi
+
+if [[ $# -ne 0 ]]; then
+    echo "Usage: $0" >&2
+    exit 1
+fi
+
+# Running "make test" is slow because CTest will spawn one process per test case, adding significant start-up costs.
+# We just call the all_tests executable manually to get around this.
+for test in cuda_bits_test sycl_bits_test encoder_test; do
+    bash /root/capture-backtrace.sh ./$test
+done
+
